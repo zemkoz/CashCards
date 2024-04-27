07eb8d13b0b703d739c661fe6fc43d830eba08d4
Zoltán Zemko <zemko_z@hotmail.com>
2024-04-27 21:31:46 +0200

    Initial commit

---
+++ src/test/java/cz/zemko/cashcards/CashCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zemko.cashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
